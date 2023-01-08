77a5173f4a480fd0cbea83679ffcad099e35fccf
VEEB <75116070+veebch@users.noreply.github.com>
2023-01-08 13:59:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ camera_config = picam2.create_preview_configuration()
 capture_config = picam2.create_still_configuration(raw={}, display=None)
 picam2.configure(camera_config)
 picam2.start_preview(Preview.NULL)
+# Exposure time
 picam2.set_controls({"ExposureTime": 10000})
+# AfMode: Set the AF mode (manual, auto, continuous)
+# LensPosition: Manual focus, Set the lens position.
+# picam2.set_controls({"AfMode": 0, "LensPosition": 100.0})
 picam2.start()
 
 time.sleep(2)
 r = picam2.switch_mode_capture_request_and_stop(capture_config)
 # r.save("main", "full.jpg")
-
-# AfMode: Set the AF mode (manual, auto, continuous)
-# LensPosition: Manual focus, Set the lens position.
-# picam2.set_controls({"AfMode": 0, "LensPosition": 100.0})
 
 savestring = "./images/capture" + str(index) + ".dng"
 r.save_dng(savestring)
